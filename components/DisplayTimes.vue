0947abc93e69c5a7d61c1579e9ceb0b76bf03a2d
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-11-24 19:27:38 -0300

    Icons, refresh button, emojis and refactoring

--- components/DisplayTimes.vue
+++ components/DisplayTimes.vue
@@ -1,5 +1,8 @@
 <script setup>
-const props = defineProps(['sunrise', 'sunset']);
+import SunAngle from '~icons/mdi/sun-angle-outline';
+import Refresh from '~icons/mdi/refresh';
+
+const props = defineProps(['weather']);
 
 const formatTime = (datetime) => {
   return new Date(datetime).toLocaleTimeString(undefined, {
@@ -8,6 +11,27 @@ const formatTime = (datetime) => {
     minute: '2-digit',
   });
 };
+
+const displaySunrise = computed(() => {
+  return formatTime(props.weather.daily.sunrise[0]);
+});
+
+const displaySunset = computed(() => {
+  return formatTime(props.weather.daily.sunset[0]);
+});
 </script>
 
-<template>{{ formatTime(sunrise) }} {{ formatTime(sunset) }}</template>
+<template>
+  <div class="text-3xl select-none">
+    <SunAngle data-flip />
+    <SunAngle />
+    {{ displaySunrise }} {{ displaySunset }}
+    <button @click="$emit('refresh')"><Refresh /></button>
+  </div>
+</template>
+
+<style>
+[data-flip] {
+  transform: scaleX(-100%);
+}
+</style>
