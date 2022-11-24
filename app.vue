3b32b62447812a50e541226154cd6bdb59e5c457
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-11-24 19:28:11 -0300

    No need to have paramaters on setWeather

--- app.vue
+++ app.vue
@@ -1,73 +1,80 @@
 <script setup>
 import { useLocalStorage } from '@vueuse/core';
 
-const currentCity = useLocalStorage('currentCity', ref({ name: '' }));
+const currentCity = useLocalStorage('currentCity', ref({}));
 const currentWeather = ref('');
 const showModal = ref(false);
 
-function setWeather(latitude, longitude, timezone) {
+function setWeather() {
   currentWeather.value = '';
-  useFetchWeather(latitude, longitude, timezone).then(
-    (response) => (currentWeather.value = response)
-  );
+  useFetchWeather(
+    currentCity.value.latitude,
+    currentCity.value.longitude,
+    currentCity.value.timezone
+  ).then((response) => (currentWeather.value = response));
 }
 
 function setCity(city = null) {
   if (city) {
     currentCity.value.name = city.name;
+    currentCity.value.region = city.admin1;
     currentCity.value.latitude = city.latitude;
     currentCity.value.longitude = city.longitude;
     currentCity.value.timezone = city.timezone;
-    setWeather(city.latitude, city.longitude, city.timezone);
+    setWeather();
   }
   showModal.value = false;
 }
 
-const displaySunrise = computed(() => {
-  return currentWeather.value.daily.sunrise[0];
-});
-
-const displaySunset = computed(() => {
-  return currentWeather.value.daily.sunset[0];
-});
-
 onMounted(() => {
   if (currentCity.value.name) {
-    setWeather(
-      currentCity.value.latitude,
-      currentCity.value.longitude,
-      currentCity.value.timezone
-    );
+    setWeather();
   }
 });
 </script>
 
 <template>
-  <div>
-    {{ currentCity.name }}
-  </div>
-
-  <Transition name="fadeIn" mode="out-in">
-    <div class="second" v-if="currentWeather">
-      <DisplayTimes :sunset="displaySunset" :sunrise="displaySunrise" />
+  <div class="app_wrapper">
+    <div class="text-center p-8">
+      <h1 class="text-3xl">
+        {{ currentCity.name }}
+      </h1>
+      <h2 class="text-lg text-blue-50/60">{{ currentCity.region }}</h2>
     </div>
-    <div v-else>LOADING WEATHER</div>
-  </Transition>
-  <button @click="showModal = true">Pick a city</button>
+    <Transition name="fadeIn" mode="out-in">
+      <template v-if="currentWeather">
+        <DisplayTimes :weather="currentWeather" @refresh="setWeather" />
+      </template>
+      <div v-else-if="currentCity.name">LOADING WEATHER</div>
+      <div v-else>...</div>
+    </Transition>
+    <button @click="showModal = true">Pick a city</button>
 
-  <Teleport to="body">
-    <SearchModal :show="showModal" @close="setCity" />
-  </Teleport>
+    <Teleport to="body">
+      <SearchModal :show="showModal" @close="setCity" />
+    </Teleport>
+  </div>
 </template>
 
 <style>
-.second {
-  color: red;
+body {
+  min-height: 100vh;
+  background-color: #000539;
+  color: #eef0f2;
+  display: grid;
+  place-content: center;
+}
+
+.app_wrapper {
+  height: 80vh;
+  width: 80vw;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
 }
 
 .fadeIn-enter-active,
 .fadeIn-leave-active {
-  transition: opacity 0.2s;
+  transition: opacity 0.1s;
 }
 
 .fadeIn-enter-from,
